0868df98578edf3752274b527ef4040afb236d4a
Dueño PASO <tu_email@ejemplo.com>
2026-01-04 02:27:44 -0500

    CHORE: Script para crear superusuario sin errores de consola

---
+++ create_admin.py
@@ -0,0 +1,19 @@
+﻿import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paso_ecosystem.settings')
+django.setup()
+
+from apps.core_saas.models import User
+
+# Configuración del Admin
+USERNAME = 'juanpablo'
+EMAIL = 'admin@pasotunja.com'
+PASSWORD = 'admin123_secure'  # Cambia esto después si quieres
+
+if not User.objects.filter(username=USERNAME).exists():
+    print(f"Creating superuser {USERNAME}...")
+    User.objects.create_superuser(USERNAME, EMAIL, PASSWORD)
+    print("✅ Superuser created successfully!")
+else:
+    print("⚠️ Superuser already exists.")
